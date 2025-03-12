3a71ad79df425b438644169b3fed484ce3b43ee8
Angel Aranguiz <angel.aranguiz@gmail.com>
2025-03-12 23:33:48 +0000

    Added reactive property binded to input, hello world message

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,10 +1,21 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+	"sap/m/MessageBox"
+], (Controller, JSONModel, MessageBox) => {
     "use strict";
 
     return Controller.extend("org.bootcamp.sapfioritest.controller.Home", {
         onInit() {
+            const oModel = new JSONModel({"InputValue": ""});
+            this.getView().setModel(oModel);
+        },
+        showMessage: function () {
+            var message = this.getView().getModel("i18n").getResourceBundle().getText("message");
+            var name = this.getView().getModel().getProperty("/InputValue");
+            if(name) message +=name;
+            else message += "world";
+            MessageBox.information(message);
         }
     });
 });
